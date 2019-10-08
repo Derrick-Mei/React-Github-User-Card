301dbdbcba32a298b046decf2db85729146a9f13
Derrick Mei <derrick.mei.168@gmail.com>
2019-10-08 02:37:41 -0500

    created react app bootstrap

---
+++ github-card-rcc/src/components/Card.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+import Follower from './Follower'
+
+export default class card extends Component {
+    render() {
+        return (
+            <div>
+                Card.js
+                <Follower />
+            </div>
+        )
+    }
+}
